5adf19da9fba768505ad81277cbedecec29d7010
DaniloSax <danilovsdanilo@gmail.com>
2021-06-01 17:15:38 -0400

    first commit

---
+++ back-end/database/migrations/2021_06_01_205306_create_accounts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('account');
+            $table->string('agency');
+            $table->string('balance');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
---
+++ back-end/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Account;
+use App\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+
+        factory(User::class, 5)->create()->each(function (User $user) {
+            factory(Account::class, 1)->create(['user_id' => $user->id]);
+        });
+    }
+}
