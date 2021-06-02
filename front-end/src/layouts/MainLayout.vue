6744964edd4614d42ba75bf8d878794768353d03
DaniloSax <danilovsdanilo@gmail.com>
2021-06-02 11:11:29 -0400

    add: start front end

--- front-end/src/layouts/MainLayout.vue
+++ front-end/src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          DESAFIO CAPGEMINI
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+          <UserDropdown/>
       </q-toolbar>
     </q-header>
 
@@ -48,6 +48,7 @@
 
 <script>
 import EssentialLink from 'components/EssentialLink.vue'
+import UserDropdown from './UserDropdown.vue'
 
 const linksData = [
   {
@@ -55,48 +56,12 @@ const linksData = [
     caption: 'quasar.dev',
     icon: 'school',
     link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
   }
 ]
 
 export default {
   name: 'MainLayout',
-  components: { EssentialLink },
+  components: { EssentialLink, UserDropdown },
   data () {
     return {
       leftDrawerOpen: false,
---
+++ front-end/src/layouts/UserDropdown.vue
@@ -0,0 +1,61 @@
+<template>
+ <q-btn-dropdown stretch flat>
+        <template v-slot:label>
+          <div class="row items-center no-wrap">
+            <q-icon left name="account_circle" />
+            <div class="text-center">username</div>
+          </div>
+        </template>
+
+        <q-list style="min-width: 100px">
+          <q-item>
+            <q-item-section>
+              <div>
+                username
+                <br />
+                <strong>Administrador</strong>
+              </div>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-item>
+            <q-item-section>
+              <span class="text-overline">Notificações:</span>
+              <div>
+                lista de Notificações aqui
+              </div>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-item clickable style="color: green;" @click="() => {}">
+            <q-item-section avatar>
+              <q-icon name="account_circle" />
+            </q-item-section>
+            <q-item-section>
+              Perfil
+            </q-item-section>
+          </q-item>
+
+          <q-item clickable style="color: red;" @click="() => {}">
+            <q-item-section avatar>
+              <q-icon name="login" />
+            </q-item-section>
+            <q-item-section>Sair</q-item-section>
+          </q-item>
+        </q-list>
+      </q-btn-dropdown>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
